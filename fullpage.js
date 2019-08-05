8d7dcdcbfacebdfa9ea3def09d295b6400ed22c9
AmirK <kooverjeeamir@gmail.com>
2019-08-05 21:46:04 +0200

    portfolio files

---
+++ fullpage.js
@@ -0,0 +1,11 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    scrollingSpeed: 1500,
+    controlArrows: true,
+	verticalCentered: false,
+	sectionsColor : ['#ccc', '#fff'],
+	paddingTop: '3em',
+	paddingBottom: '10px',
+	
+})
+
